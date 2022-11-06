061afd0bd1037f9a70f7ae16fb804181bd3cfddc
Arturo Gonzalez Breton <arturo.gbreton@gmail.com>
2022-11-05 18:01:38 -0600

    Add lector de base de datos

--- app/src/main/java/com/example/reproductormp3/Minero.kt
+++ app/src/main/java/com/example/reproductormp3/Minero.kt
@@ -1,14 +1,158 @@
 package com.example.reproductormp3
 
 import java.io.File
+import android.media.MediaMetadataRetriever
+import android.database.sqlite.SQLiteDatabase
+import android.icu.util.Calendar
+import java.time.Year
 
+/**
+ * Clase para buscar canciones en el dispositivo y agregarlas a la
+ * base de datos.
+ */
 class Minero {
 
-    fun mina(path: String): MutableList<String> {
-        var songs = mutableListOf<String>()
+    /**
+     * Busca canciones en el directorio recibido y sus
+     * subdirectorios.
+     */
+    fun mina(path: String, bdd: SQLiteDatabase): MutableList<String?> {
+        val lector = MediaMetadataRetriever()
+        val songs = mutableListOf<String?>()
         File(path).walk().forEach {
-            songs.add(it.toString())
+            try {
+                lector.setDataSource(it.toString())
+                var artist = lector.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+                var title = lector.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
+                var album = lector.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM)
+                var time = lector.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DATE)
+                var genre = lector.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE)
+                var track = lector.extractMetadata(MediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER)
+
+                if (artist == null)
+                    artist = "Unknown"
+                if (album == null)
+                    album = "Unknown"
+                if (title == null)
+                    title = "Unknown"
+                if (time == null)
+                    time = "0"
+                if (genre == null)
+                    genre = "Unknown"
+                if (track == null)
+                    track = "0"
+
+                if (!existeArtista(artist, bdd))
+                    agregaArtista(artist, bdd)
+
+                if (!existeAlbum(album, bdd))
+                    agregaAlbum(album, bdd)
+
+                if (!existePath(it.toString(), bdd))
+                    agregaCancion(artist, album, it.toString(), title, track, time, genre, bdd)
+
+            } catch (iae: IllegalArgumentException) {
+            }
         }
         return songs
+    }
+
+    /**
+     * Determina si existe el artista en la base de datos.
+     */
+    private fun existeArtista(artista: String?, bdd: SQLiteDatabase): Boolean {
+        val cursor = bdd.rawQuery("SELECT name FROM performers WHERE ?=?",
+                                        arrayOf("name", artista))
+        if (cursor.count > 0) {
+            cursor.close()
+            return true
+        }
+        return false
+    }
+
+    /**
+    * Determina si existe el álbum en la base de datos.
+    */
+    private fun existeAlbum(album: String?, bdd: SQLiteDatabase): Boolean {
+        val cursor = bdd.rawQuery("SELECT name FROM performers WHERE ?=?",
+                                        arrayOf("name", album))
+        if (cursor.count > 0) {
+            cursor.close()
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Determina si el archivo ya está en la base de datos.
+     */
+    private fun existePath(path: String?, bdd: SQLiteDatabase): Boolean {
+        val cursor = bdd.rawQuery("SELECT name FROM performers WHERE ?=?",
+                                        arrayOf("path", path))
+        if (cursor.count > 0) {
+            cursor.close()
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Agrega un álbum a la base de datos.
+     */
+    private fun agregaAlbum(album: String?, bdd: SQLiteDatabase) {
+        val albums = bdd.rawQuery("SELECT name FROM albums", emptyArray())
+        val numAlbums = albums.count
+        bdd.execSQL("INSERT INTO performers VALUES($numAlbums, 2, $album)")
+        albums.close()
+    }
+
+    /**
+     * Agrega un artista a la base de datos.
+     */
+    private fun agregaArtista(artista: String?, bdd: SQLiteDatabase) {
+        val performers = bdd.rawQuery("SELECT name FROM performers", emptyArray())
+        val numPerformers = performers.count
+        bdd.execSQL("INSERT INTO performers VALUES($numPerformers, 2, $artista)")
+        performers.close()
+    }
+
+    /**
+     * Agrega una canción a la base de datos.
+     */
+    private fun agregaCancion(artist: String?,
+                              album: String?,
+                              path: String?,
+                              title: String?,
+                              track: String?,
+                              time: String?,
+                              genre: String?, bdd: SQLiteDatabase) {
+        var cursor = bdd.rawQuery("SELECT id_rola FROM rolas", emptyArray())
+        val numSongs = cursor.count
+
+        cursor = bdd.rawQuery("SELECT id_performer FROM performers WHERE ?=?",
+                                    arrayOf("name", artist))
+        var idPerformer: String?
+        idPerformer = "null"
+        if (cursor.moveToNext()) {
+            idPerformer = cursor.getString(cursor.getColumnIndexOrThrow("id_performer"))
+        }
+
+        cursor = bdd.rawQuery("SELECT id_album FROM performers WHERE ?=?",
+            arrayOf("name", album))
+        var idAlbum: String?
+        idAlbum = "null"
+        if (cursor.moveToNext()) {
+            idAlbum = cursor.getString(cursor.getColumnIndexOrThrow("id_album"))
+        }
+        cursor.close()
+
+        bdd.execSQL("INSERT INTO rolas VALUES($numSongs, " +
+                "                                 $idPerformer, " +
+                "                                 $idAlbum, " +
+                "                                 $path, " +
+                "                                 $title," +
+                "                                 $track," +
+                "                                 $time" +
+                "                                 $genre)")
     }
 }
--- app/src/main/java/com/example/reproductormp3/SQLite.kt
+++ app/src/main/java/com/example/reproductormp3/SQLite.kt
@@ -9,40 +9,41 @@ class SQLite(
     factory: SQLiteDatabase.CursorFactory?,
     version: Int
 ) : SQLiteOpenHelper(context, name, factory, version) {
+
     override fun onCreate(db: SQLiteDatabase?) {
-        val sql = "CREATE TABLE types (" +
+        val types = "CREATE TABLE types (" +
                 "   id_type               INTEGER PRIMARY KEY," +
                 "   description           TEXT" +
-                " )" +
-                "INSERT INTO types VALUES(0, 'Person')" +
-                "INSERT INTO types VALUES(1, 'Group')" +
-                "INSERT INTO types VALUES(2, 'Unknown')" +
-                "CREATE TABLE performers (" +
+                " );"
+        val insertPerson = "INSERT INTO types VALUES(0, 'Person');"
+        val insertGroup = "INSERT INTO types VALUES(1, 'Group');"
+        val insertUnknown = "INSERT INTO types VALUES(2, 'Unknown');"
+        val performers = "CREATE TABLE performers (" +
                 "   id_performer          INTEGER PRIMARY KEY," +
                 "   id_type               INTEGER," +
                 "   name                  TEXT," +
                 "   FOREIGN KEY (id_type) REFERENCES types(id_type)" +
-                ")" +
-                "CREATE TABLE persons (" +
+                ");"
+        val persons = "CREATE TABLE persons (" +
                 "   id_person             INTEGER PRIMARY KEY," +
                 "   stage_name            TEXT," +
                 "   real_name             TEXT," +
                 "   birth_date            TEXT," +
                 "   death_date            TEXT" +
-                ")" +
-                "CREATE TABLE groups (" +
+                ");"
+        val groups = "CREATE TABLE groups (" +
                 "   id_group              INTEGER PRIMARY KEY," +
                 "   name                  TEXT," +
                 "   start_date            TEXT," +
                 "   end_date              TEXT" +
-                ")" +
-                "CREATE TABLE albums (" +
+                ");"
+        val albums = "CREATE TABLE albums (" +
                 "   id_album              INTEGER PRIMARY KEY," +
                 "   path                  TEXT," +
                 "   name                  TEXT," +
                 "   year                  INTEGER" +
-                ")" +
-                "CREATE TABLE rolas (" +
+                ");"
+        val rolas = "CREATE TABLE rolas (" +
                 "   id_rola               INTEGER PRIMARY KEY," +
                 "   id_performer          INTEGER," +
                 "   id_album              INTEGER," +
@@ -53,15 +54,25 @@ class SQLite(
                 "   genre                 TEXT," +
                 "   FOREIGN KEY (id_performer) REFERENCES performers(id_performer)," +
                 "   FOREIGN KEY (id_album)     REFERENCES albums(id_album)" +
-                ")" +
-                "CREATE TABLE in_group (" +
+                ");"
+        val in_group = "CREATE TABLE in_group (" +
                 "   id_person             INTEGER," +
                 "   id_group              INTEGER," +
                 "   PRIMARY KEY (id_person, id_group)," +
                 "   FOREIGN KEY (id_person)    REFERENCES persons(id_person)," +
-                "   FOREIGN KEY (id_group)     REFERENCES groups(id_group),"
+                "   FOREIGN KEY (id_group)     REFERENCES groups(id_group)" +
+                ");"
 
-        db?.execSQL(sql)
+        db?.execSQL(types)
+        db?.execSQL(insertPerson)
+        db?.execSQL(insertGroup)
+        db?.execSQL(insertUnknown)
+        db?.execSQL(performers)
+        db?.execSQL(persons)
+        db?.execSQL(groups)
+        db?.execSQL(albums)
+        db?.execSQL(rolas)
+        db?.execSQL(in_group)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {

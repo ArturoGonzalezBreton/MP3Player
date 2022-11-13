5cba851df074f7949f4307c23902c34e24aa3726
Arturo Gonzalez Breton <arturo.gbreton@gmail.com>
2022-11-13 11:42:19 -0600

    Add lenguaje de búsquedas

--- app/src/main/java/com/example/reproductormp3/Lector.kt
+++ app/src/main/java/com/example/reproductormp3/Lector.kt
@@ -25,8 +25,51 @@ class Lector {
                 emptyArray())
         }
 
-        fun busca(bdd: SQLiteDatabase?, string: String) {
-
+        fun busca(bdd: SQLiteDatabase?, busqueda: String): Cursor? {
+            if (bdd == null)
+                return null
+            var sql = "SELECT title," +
+                    " album_name, " +
+                    " performer_name, " +
+                    " rolas.year, " +
+                    " genre, " +
+                    " rolas.path FROM rolas" +
+                    " JOIN albums" +
+                    " JOIN performers ON" +
+                    " rolas.id_album = albums.id_album AND" +
+                    " rolas.id_performer = performers.id_performer WHERE "
+            if (!busqueda.contains(":")) {
+                sql += " title = '$busqueda'"
+            } else {
+                val pattern = " ".toRegex()
+                val criterios = pattern.split(busqueda)
+                criterios.forEach {
+                    when (it.substringBefore(":").trim()) {
+                        "song" -> {
+                            sql += " title LIKE '%${it.substringAfter(":").trim()}%'"
+                        }
+                        "year" -> {
+                            sql += " year ${it.substringAfter(":").trim()}"
+                        }
+                        "artist" -> {
+                            sql += " artist LIKE '%${it.substringAfter(":").trim()}%'"
+                        }
+                        "album" -> {
+                            sql += " album LIKE '%${it.substringAfter(":").trim()}%'"
+                        }
+                        "genre" -> {
+                            sql += " genre LIKE '%${it.substringAfter(":").trim()}%'"
+                        }
+                        "&" -> {
+                            sql += " AND "
+                        }
+                        "|" -> {
+                            sql += " OR "
+                        }
+                    }
+                }
+            }
+            return bdd.rawQuery(sql, null)
         }
     }
 }
